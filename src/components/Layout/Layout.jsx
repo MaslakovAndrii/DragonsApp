67590e664ea666dadcb2ed198dd5b0b65d8ed196
Andrii Maslakov <web.work.mv@gmail.com>
2022-09-23 09:54:58 +0300

    Добавил в компонент Layout хук авторизации

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuth } from '../../hooks/useAuth';
+import { AUTHORIZATION_ROUTE } from '../../utils/const';
 import Header from '../Header/Header';
 
 const Layout = () => {
+     const { isAuth } = useAuth()
 
-     return (
-          <>
-               <Header />
-               <Outlet />
-          </>
-     )
+     return isAuth
+          ? (<>
+               <Header/>
+               <Outlet/>
+          </>)
+          : (
+               <Navigate to={AUTHORIZATION_ROUTE} />
+          )
 };
 
 export default Layout;
