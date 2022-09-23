17574a711d0bac1a00e2814b89999d75ec232eb8
Andrii Maslakov <web.work.mv@gmail.com>
2022-09-23 10:32:14 +0300

    Создал компоненты "заглушки" DragonsList и DragonItem

---
+++ src/components/DragonItem/DragonItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DragonItem = () => {
+     return (
+          <div>
+               DragonItem
+          </div>
+     );
+};
+
+export default DragonItem;
---
+++ src/components/DragonsList/DragonsList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DragonsList = () => {
+     return (
+          <div>
+               DragonsList
+          </div>
+     );
+};
+
+export default DragonsList;
