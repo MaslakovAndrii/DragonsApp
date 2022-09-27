f4a741e6c68b07fa00c8d8934edb61ff43cceaa1
Andrii Maslakov <web.work.mv@gmail.com>
2022-09-27 16:33:58 +0300

    Добавил стили для header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { removeUser } from '../../store/slices/userSlice';
 import { LIST_ROUTE } from '../../utils/const';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import Logo from '../../assets/image/logo.png'
+
+import './Header.scss'
 
 
 const Header = () => {
+     const [menuOpen, setMenuOpen] = useState(false)
      const dispatch = useDispatch()
+     const navigate = useNavigate()
+
+     const handleMenuOpen = () => {
+          if(!menuOpen) {
+               setMenuOpen(true)
+          } else {
+               setMenuOpen(false)
+          }
+
+     }
 
      return (
-          <div>
-               <div>Dragon</div>
-               <nav>
-                    <ul>
-                         <li><NavLink to={LIST_ROUTE}>Список ракет</NavLink></li>
-                    </ul>
-               </nav>
-               <button onClick={() => dispatch(removeUser())}>Выйти</button>
-          </div>
+          <header className='header'>
+               <img className='header__logo logo' src={Logo} onClick={() => navigate('/')}/>
+               <div className={`header__body ${menuOpen ? 'menu-open' : ''}`}>
+                    <nav className='header__menu menu'>
+                         <ul className='menu__list'>
+                              <li className='menu__item'><NavLink className='link link_dark' to={LIST_ROUTE}>Список ракет</NavLink></li>
+                              <li className='menu__item'><NavLink className='link link_dark' to={LIST_ROUTE}>Список ракет</NavLink></li>
+                         </ul>
+                    </nav>
+                    <button className='header__logout logout btn' onClick={() => dispatch(removeUser())}>Выйти</button>
+               </div>
+               <GiHamburgerMenu onClick={() => handleMenuOpen()} className='header__burger burger'/>
+          </header>
      );
 };
 
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,16 +2,26 @@ import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth';
 import { AUTHORIZATION_ROUTE } from '../../utils/const';
+import Container from '../Container/Container';
 import Header from '../Header/Header';
+import bgImage from '../../assets/image/bg.jpg'
 
 const Layout = () => {
      const { isAuth } = useAuth()
 
      return isAuth
-          ? (<>
-               <Header/>
-               <Outlet/>
-          </>)
+          ? (<div style={{
+               height: '100vh',
+               backgroundImage: `url(${bgImage})`,
+               backgroundPosition: 'center',
+               backgroundSize: 'cover',
+               backgroundRepeat: 'no-repeat',
+          }}>
+               <Container size='max'>
+                    <Header />
+                    <Outlet />
+               </Container>
+          </div>)
           : (
                <Navigate to={AUTHORIZATION_ROUTE} />
           )
