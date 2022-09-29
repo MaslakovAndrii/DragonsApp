f9ff0650ebb338c67280991faeb8c1a73512e4e0
Andrii Maslakov <web.work.mv@gmail.com>
2022-09-29 23:21:38 +0300

    Реализовал слайс для добавления и удаления из избранного

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from './slices/userSlice'
+import favoriteReducer from './slices/userSlice'
 
 const store = configureStore({
      reducer: {
           user: userReducer,
+          favorite: favoriteReducer
      }
 })
 
---
+++ src/store/slices/favoriteSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = []
+
+const favoriteSlice = createSlice({
+     name: 'favorite',
+     initialState,
+     reducers: {
+          setFavorite(state, action) {
+               if(!state.includes(action.payload)) {
+                    state.push(action.payload)
+               }
+          },
+          removeFavorite(state, action) {
+               const current = state.findIndex((item) => {
+                    item === action.payload
+               })
+               state = [...state.slice(0, current), ...state(current + 1, state.length)]
+          }
+     }
+});
+
+export const { setFavorite, removeFavorite} = favoriteSlice.actions;
+
+export default favoriteSlice.reducer;
