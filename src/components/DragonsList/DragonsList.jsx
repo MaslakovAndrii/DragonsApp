f48e92b812220a69d940b8291610fb007c181ff2
Andrii Maslakov <web.work.mv@gmail.com>
2022-09-29 19:27:11 +0300

    Закончил работу над стилями в компоненте DragonsList

--- src/components/DragonsList/DragonsList.jsx
+++ src/components/DragonsList/DragonsList.jsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Container from '../Container/Container'
 import Loader from '../Loader/Loader';
+import Pagination from '../Pagination/Pagination';
+import { FcLike } from 'react-icons/fc'
 import './DragonList.scss'
 
 const DragonsList = () => {
@@ -37,21 +39,30 @@ const DragonsList = () => {
      }
 
      return (
-          <Container size='small'>
-               <div className='content'>
-                    <dl className='dragon-list'>
-                         {data.docs.map(dragon => (
-                              <Link to={`${dragon.id}`} className='dragon-list__link' key={dragon.id}>
-                                   <dt className='dragon-list__title'><img className='dragon-list__img' src={dragon.flickr_images[0]} alt='картинки нет'></img></dt>
-                                   <dd className='dragon-list__description'>{dragon.name}</dd>
-                              </Link>
-                         ))}
-                    </dl>
-               </div>
-               <div style={{display: 'flex'}}>
-                    {data.totalPages > 1 ? [...Array(data.totalPages)].map((item, index) => <button key={index} onClick={() => handlePage(index+1)}>{index+1}</button> ) : null}
-               </div>
-          </Container>
+          <div className='dragon-list'>
+               {/* <Container size='max'> */}
+                    <div className='dragon-list__inner'>
+                         <div className='dragon-list__content'>
+                              <ul className='dragon-list__list list-dragon'>
+                                   {data.docs.map(dragon => (
+                                        <li className='list-dragon__item' key={dragon.id}>
+                                             <Link to={`${dragon.id}`} className='list-dragon__link link' key={dragon.id}>
+                                                  <div className='list-dragon__img-container'><img className='list-dragon__img' src={dragon.flickr_images[0]} alt='картинки нет'/></div>
+                                                  <h4 className='list-dragon__title'>{dragon.name}</h4>
+                                                  <p className='list-dragon__description'>{dragon.description}</p>
+                                             </Link>
+                                             <button className='list-dragon__btn btn_null'>
+                                                  <FcLike className='list-dragon__like' />
+                                                  <span className='tooltip'>Add to favorites</span>
+                                             </button>
+                                        </li>
+                                   ))}
+                              </ul>
+                         </div>
+                         <Pagination pages={data.totalPages} handlePage={handlePage} pageActive={data.page} />
+                    </div>
+               {/* </Container> */}
+          </div>
      );
 };
 
