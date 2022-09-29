6921d8c995dbff922902ec63176855cf7b2c7015
Andrii Maslakov <web.work.mv@gmail.com>
2022-09-29 17:22:09 +0300

    Сделал простой компонент Pagination. В идеале еще доработать по типу слайдера (если будет время)

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Pagination.scss'
+
+const Pagination = ({ pages, pageActive, handlePage }) => {
+
+
+     return (
+          <div className='pagination'>
+               <div className='pagination__container'>
+                    {pages > 1
+                         ? [...Array(pages)].map((item, index) => <button
+                              className={`pagination__page btn_null ${pageActive === index + 1 ? 'page-active' : null}`}
+                              key={index}
+                              onClick={() => handlePage(index + 1)}
+                         >
+                              {index + 1}
+                         </button>)
+                         : null}
+               </div>
+          </div>
+     );
+};
+
+export default Pagination;
