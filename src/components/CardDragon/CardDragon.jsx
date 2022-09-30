535c5e4f45bcfeab0040ceeddc4aa7188d792065
Andrii Maslakov <web.work.mv@gmail.com>
2022-09-30 10:21:37 +0300

    Добавил функционал добавления в избранное в CardDragon

--- src/components/CardDragon/CardDragon.jsx
+++ src/components/CardDragon/CardDragon.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FcLike } from 'react-icons/fc'
 import './CardDragon.scss'
+import { useDispatch } from 'react-redux';
+import { setFavorite } from '../../store/slices/favoriteSlice';
 
 const CardDragon = ({ id, image, name, description }) => {
+     const dispatch = useDispatch()
+
+     const addToFavorites = () => {
+          // console.log(id);
+          dispatch(setFavorite(id))
+     }
+
      return (
           <article className='card-dragon'>
                <Link to={`${id}`} className='card-dragon__link link' >
@@ -11,7 +20,7 @@ const CardDragon = ({ id, image, name, description }) => {
                     <h4 className='card-dragon__title'>{name}</h4>
                     <p className='card-dragon__description'>{description}</p>
                </Link>
-               <button className='card-dragon__btn btn_null'>
+               <button  className='card-dragon__btn btn_null' onClick={() => addToFavorites(id)}>
                     <FcLike className='card-dragon__like' />
                     <span className='tooltip'>Add to favorites</span>
                </button>
