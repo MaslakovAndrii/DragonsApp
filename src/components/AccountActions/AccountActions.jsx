3c4707d5c962aa2c895c8282b169b26be2d4b70f
Andrii Maslakov <web.work.mv@gmail.com>
2022-09-30 13:17:15 +0300

    Добавил модальное окно в AccountActions

--- src/components/AccountActions/AccountActions.jsx
+++ src/components/AccountActions/AccountActions.jsx
@@ -4,10 +4,12 @@ import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { removeUser } from '../../store/slices/userSlice';
 import { FAVORITE_ROUTE } from '../../utils/const';
+import Modal from '../Modal/Modal'
 import './AccountActions.scss'
 
 const Profile = () => {
      const [visibleBottom, setVisibleBottom] = useState(false)
+     const [modalActive, setModalActive] = useState(false)
      const dispatch = useDispatch()
 
      const handleProfile = () => {
@@ -28,13 +30,14 @@ const Profile = () => {
                </div>
                <div className={`profile__bottom ${visibleBottom ? 'profile__bottom_visible' : null}`}>
                     <ul className='profile__list list-profile'>
-                         <li className='list-profile__item link'>Profile</li>
+                         <li className='list-profile__item'><button onClick={() => setModalActive(true)} className='list-profile__btn btn_null'>Profile</button></li>
                          <li className='list-profile__item'>
                               <Link className='list-profile__link link' to={FAVORITE_ROUTE}>Favorite</Link>
                          </li>
                     </ul>
                     <button className='header__logout logout btn' onClick={() => dispatch(removeUser())}>Выйти</button>
                </div>
+               <Modal active={modalActive} setActive={setModalActive}>hello</Modal>
           </div>
      );
 };
