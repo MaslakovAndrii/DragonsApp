30cb48e004dc3077bd43e37f018c94d9a50c9d94
Andrii Maslakov <web.work.mv@gmail.com>
2022-10-04 15:34:02 +0300

    Исправил стили у ссылок. Теперь они все inline-block, а в меню акаунта шириной 100% чтобы кликать можно было по всей ширине.

--- src/components/AccountActions/AccountActions.jsx
+++ src/components/AccountActions/AccountActions.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { AUTHORIZATION_ROUTE, FAVORITE_ROUTE } from '../../utils/const';
 
